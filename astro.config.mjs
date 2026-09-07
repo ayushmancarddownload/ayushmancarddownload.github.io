cd4b574496721e5d9b5c5b4ee9501f1a1942b4b2
ubg2028 <308474343+ubg2028@users.noreply.github.com>
2026-09-07 09:21:11 +0530

    p

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://ayushmancarddownload.github.io', // or your custom domain
-  integrations: [sitemap()],
+  integrations: [tailwind(), sitemap()],
+  site: 'https://ubg2028.github.io',
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
 });
